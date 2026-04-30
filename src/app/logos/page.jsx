56c6a9261c9e1f1dc6a96b041bb070dc8d63b9b7
Talha-Ahmed82 <ta8170407@gmail.com>
2026-05-01 04:08:36 +0500

    updated

--- src/app/logos/page.jsx
+++ src/app/logos/page.jsx
@@ -1,21 +1,16 @@
 "use client";
 import HeroSection from "../components/logos/HeroSection";
 import AwardsSection from "../components/Banners";
-import AboutSection from "../components/AboutSection";
-import BuiltSection from "../components/BuiltSection";
-import ProcessSection from "../components/ProcessSection";
 import IndustryInsights from "../components/IndustryInsights";
 import ContactSection from "../components/ContactSection";
-import BrandsSection from "../components/about/BrandsSection";
-import FAQSection from "../components/about/FAQSection";
 import BuildSection from "../components/logos/BuildSection";
 import Apologies from "../components/logos/Apologies";
 import PurposeSection from "../components/logos/PurposeSection";
 import WorkSection from "../components/logos/Work";
-import DirectionSection from "../components/logos/Direction";
 import Holds from "../components/logos/Holds";
 import TestimonialSection from "../components/TestimonialSection";
 import PricingSection from "../components/logos/Pricing";
+import DirectionSection from "../components/logos/Direction";
 
 export default function About() {
   return (
@@ -26,6 +21,7 @@ export default function About() {
      <Apologies />
      <PurposeSection />
      <WorkSection />
+     <DirectionSection />
      <Holds />
      <TestimonialSection />
      <PricingSection />
